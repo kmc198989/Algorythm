bae76c8d60bb66644a9dd9930fda14c3d81887b9
kmc198989 <97355232+kmc198989@users.noreply.github.com>
2025-03-18 20:41:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.55 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,22 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
         Map<String, Integer> map = new HashMap<>();
+        
         for (String player : participant) {
             map.put(player, map.getOrDefault(player, 0) + 1);
         }
-        for (String player : completion) {
-            map.put(player, map.get(player) - 1);
-        }  
-        for (String player : participant) {
-            if (map.get(player) == 1) return player;
+        
+        for (String player2 : completion) {
+            map.put(player2, map.get(player2) - 1);
         }
-        return "";
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) return entry.getKey();
+        }
+        
+        return answer;
     }
 }
