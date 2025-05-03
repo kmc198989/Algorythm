77029ff38aa42b77f08f97d21e35e61d09f376a0
kmc198989 <97355232+kmc198989@users.noreply.github.com>
2025-05-03 18:50:28 +0900

    [level 1] Title: 소수 찾기, Time: 13.64 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,24 +1,17 @@
-import java.util.*;
-
-class Solution {
-    boolean[] all;
+public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        all = new boolean[n + 1];
+        boolean[] isComposite = new boolean[n + 1];
+        int count = 0;
+
         for (int i = 2; i <= n; i++) {
-            if (isPrime(i)) answer++;
+            if (!isComposite[i]) {
+                count++;
+                for (int j = i * 2; j <= n; j += i) {
+                    isComposite[j] = true;
+                }
+            }
         }
-        
-        return answer;
-    }
-    
-    private boolean isPrime(int n) {
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
-        
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) return false;
-        }
-        return true;
+
+        return count;
     }
 }
