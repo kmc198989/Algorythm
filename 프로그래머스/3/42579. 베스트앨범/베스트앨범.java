f08de2811377beac92feb8c3eb0a20d8a0eec55d
kmc198989 <97355232+kmc198989@users.noreply.github.com>
2025-09-23 22:47:53 +0900

    [level 3] Title: 베스트앨범, Time: 5.54 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,62 +1,46 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        Map<String, Integer> genre = new HashMap<>();
+        Map<String, PriorityQueue<Song>> songs = new HashMap<>();
+        
         int len = genres.length;
-        Map<String, Integer> playmap = new HashMap<>();
-        Map<String, PriorityQueue<Song>> genremap = new HashMap<>();
         
         for (int i = 0; i < len; i++) {
             String gen = genres[i];
-            int play = plays[i];
-            playmap.put(gen, playmap.getOrDefault(gen, 0) + play);
-            genremap.computeIfAbsent(gen, k -> new PriorityQueue<>((a, b) -> {
-                if (a.play != b.play) return b.play - a.play;
-                return a.idx - b.idx;
-            })).add(new Song(play, i));
+            int pl = plays[i];
+            genre.put(gen, genre.getOrDefault(gen, 0) + pl);
+            songs.computeIfAbsent(gen, s -> new PriorityQueue<>((a, b) -> {
+                if (a.play != b.play) {
+                    return Integer.compare(b.play, a.play);
+                } else {
+                    return Integer.compare(a.index, b.index);
+                }
+            })).add(new Song(i, pl));            
         }
-        
-        PriorityQueue<Genre> pq = new PriorityQueue<>((a, b) -> b.play - a.play);
-        
-        for (Map.Entry<String, Integer> entry : playmap.entrySet()) {
-            pq.add(new Genre(entry.getValue(), entry.getKey()));
-        }
-        
-        List<Integer> result = new ArrayList<>();
 
-        while (!pq.isEmpty()) {
-            String genre = pq.poll().name;
-            PriorityQueue<Song> songs = genremap.get(genre);
-            int length = Math.min(2, songs.size());
-            for (int i = 0; i < length; i++) {
-                result.add(songs.poll().idx);
+        List<String> g = new ArrayList<>(genre.keySet());
+        g.sort((a, b) -> Integer.compare(genre.get(b), genre.get(a)));
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < g.size(); i++) {
+            String curgen = g.get(i);
+            PriorityQueue<Song> pq = songs.get(curgen);
+            for (int j = 0; j < 2 && !pq.isEmpty(); j++) {
+                result.add(pq.poll().index);
             }
-        }        
-        answer = new int[result.size()];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = result.get(i);
         }
         
-        return answer;
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
     
-    class Song {
+    public class Song {
+        int index;
         int play;
-        int idx;
-        Song(int play, int idx) {
+        
+        Song(int index, int play) {
+            this.index = index;
             this.play = play;
-            this.idx = idx;
-        }
-    }
-    
-    class Genre {
-        int play;
-        String name;
-        Genre(int play, String name) {
-            this.play = play;
-            this.name = name;
         }
     }
 }
